378ee74d46263c04914cb16809e0434635f75d95
Zain Ul Hassan <zainulhassan4330@gmail.com>
2021-10-26 01:57:44 +0500

    Initial release

---
+++ doczrc.js
@@ -0,0 +1,32 @@
+export default {
+  title: "Dreamers Lab",
+  public: "/public",
+  dest: "/build",
+  base: "/docs",
+  propsParser: false,
+  themeConfig: {
+    search: true,
+    header: {
+      icons: "minimal",
+    },
+    menu: {
+      search: false,
+      headings: {
+        rightSide: true,
+        scrollspy: true,
+        depth: 3,
+      },
+    },
+    logo: {
+      src: {
+        light: "/public/logo-light.svg",
+        dark: "/public/logo-dark.svg",
+      },
+      width: 50,
+    },
+  },
+  groups: {
+    "": [],
+    Projects: ["Extensions"],
+  },
+};
